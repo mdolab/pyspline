502dd7805b8a97af13f43752d358fb17b8d19f00
Gaetan Kenway <gaetank@gmail.com>
2009-05-23 22:04:39 +0000

    added the pySpline directory which consists of real and complex versions of the cmlib bspline functions

---
+++ f2pyit.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+#Makefile for pySpline
+
+#First make the source files for the real version
+cd src
+
+make
+
+cd ../
+
+#Now f2py just the two functions we need
+f2py  --fcompiler=intel --f90flags=-r8 -c -m pyspline src/tensbs/b2ink.f src/tensbs/b2val.f src/libtensbs.a
+
+#Now make the source files for the complex version
+
+mv pyspline.so ./python
+
+cd src_cs
+
+make
+
+cd ../
+
+f2py  -c --fcompiler=intel  --f90flags=-r8 -I./src_cs -m pyspline_cs src_cs/tensbs/c_b2ink.f src_cs/tensbs/c_b2val.f src_cs/libtensbs_cs.a
+
+mv pyspline_cs.so ./python
