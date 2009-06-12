33bc8ef83a0d496210e3b68a9306416d17a6feeb
kenway <kenway@localhost>
2009-06-12 20:25:06 +0000

    update to the 2D opt_examples

--- f2pyit.sh
+++ f2pyit.sh
@@ -9,7 +9,7 @@ make
 cd ../
 
 #Now f2py just the functions we need in the pyf
-f2py  --fcompiler=ifort --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libtensbs.a
+f2py  --fcompiler=ifort --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libtensbs.a -llapack
 
 #Now make the source files for the complex version
 
@@ -21,6 +21,5 @@ make
 
 cd ../
 
-f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libtensbs_cs.a
-
+f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libtensbs_cs.a 
 mv pyspline_cs.so ./python
