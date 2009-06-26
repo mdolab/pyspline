1706784f554e1352189e34af627cc4b8c314e0d6
kenway <kenway@localhost>
2009-06-26 20:19:48 +0000

    small update

--- f2pyit.sh
+++ f2pyit.sh
@@ -9,17 +9,17 @@ make
 cd ../
 
 #Now f2py just the functions we need in the pyf
-f2py  --fcompiler=ifort --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libtensbs.a -llapack
+f2py  --fcompiler=gfortran --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libtensbs.a -llapack
 
 #Now make the source files for the complex version
 
 mv pyspline.so ./python
 
-cd src_cs
+# cd src_cs
 
-make
+# make
 
-cd ../
+# cd ../
 
-f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libtensbs_cs.a 
-mv pyspline_cs.so ./python
+# f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libtensbs_cs.a 
+# mv pyspline_cs.so ./python
