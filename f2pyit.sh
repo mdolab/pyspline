4b3d7c0f0889ef6760dc8988a7c3e25db07f4736
kenway <kenway@localhost>
2009-08-26 20:01:44 +0000

    pyGeo update

--- f2pyit.sh
+++ f2pyit.sh
@@ -12,7 +12,7 @@ fi
 cd ../
 
 #Now f2py just the functions we need in the pyf
-f2py  --fcompiler=intel --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libtensbs.a -llapack
+f2py  --fcompiler=intel --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libspline.a
 
 #Now make the source files for the complex version
 
@@ -24,5 +24,5 @@ make
 
 cd ../
 
-f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libtensbs_cs.a 
+f2py  --fcompiler=intel --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libspline_cs.a 
 mv pyspline_cs.so ./python
