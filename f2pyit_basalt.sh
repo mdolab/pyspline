addac79fadd0cb4efdb5cfdfd6f9cd1902992afd
kenway <kenway@localhost>
2009-10-16 19:48:26 +0000

    pyGeo/pyLayout update

--- f2pyit_basalt.sh
+++ f2pyit_basalt.sh
@@ -1,26 +1,30 @@
- #/bin/bash
+#/bin/bash
 #Makefile for pySpline
 
 #First make the source files for the real version
 cd src
 
 make
+if [ ! $? -eq 0 ]; then
+    exit
+fi
 
 cd ../
 
-#Now f2py just the two functions we need
+#Now f2py just the functions we need in the pyf
 f2py  --fcompiler=intele --f90flags=-r8 -c -m pyspline src/pyspline.pyf src/libspline.a
-
-#Now make the source files for the complex version
-
 mv pyspline.so ./python
+
+# #Now make the source files for the complex version
 
 cd src_cs
 
 make
+if [ ! $? -eq 0 ]; then
+    exit
+fi
 
 cd ../
 
-f2py  --fcompiler=intele --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libspline_cs.a
-
+f2py  --fcompiler=intele --f90flags=-r8 -I./src_cs -c -m pyspline_cs src_cs/pyspline_cs.pyf src_cs/libspline_cs.a 
 mv pyspline_cs.so ./python
