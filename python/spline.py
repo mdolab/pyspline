7fd7b20d365895f5983a7377b1c90c670a49adfd
kenway <kenway@localhost>
2010-01-14 22:01:45 +0000

    added lsqr.py an iterative mehtod for least squares---testing

--- python/spline.py
+++ python/spline.py
@@ -8,7 +8,7 @@ from mdo_import_helper import *
 from scipy.io import savemat
 from matplotlib.pylab import plot,show,axis
 import copy
-
+from lsqr import *
 
 nacax,nacay = loadtxt('naca0012.dat',unpack=True)
 
@@ -28,41 +28,99 @@ nacax,nacay = loadtxt('naca0012.dat',unpack=True)
 #print 'knots:',knots
 #print 'coef:',coef
 
-#Test the compute curve
-k = 3
-Nctl = 3
-n = 25
-theta = linspace(0,pi/2,n)
-x = cos(theta)
-y = sin(theta)
-X = vstack([x,y]).T.astype('d')
+# #Test the compute curve
+# k = 3
+# Nctl = 3
+# n = 25
+# theta = linspace(0,pi/2,n)
+# x = cos(theta)
+# y = sin(theta)
+# X = vstack([x,y]).T.astype('d')
+# s0 = linspace(0,1,n)
+# knots = pyspline.knots(s0,Nctl,k)
+# s = copy.deepcopy(s0)
+# s,coef = pyspline.compute_curve(s,X,knots,k,Nctl)
+# print coef
+
+#sys.exit(0)
+k = 4
+Nctl = 40
+n = len(nacax)
+X = vstack([nacax,nacay]).T.astype('d')
 s0 = linspace(0,1,n)
 knots = pyspline.knots(s0,Nctl,k)
 s = copy.deepcopy(s0)
-s,coef = pyspline.compute_curve(s,X,knots,k,Nctl)
-print coef
+#s,coef = pyspline.compute_curve(s,X,knots,k,Nctl)
+coef = zeros((Nctl,2))
+coef[:,1] = 1.0
 
-#sys.exit(0)
-# k = 4
-# Nctl = 10
-# n = len(nacax)
-# X = vstack([nacax,nacay]).T.astype('d')
-# s0 = linspace(0,1,n)
-# knots = pyspline.knots(s0,Nctl,k)
-# #print 's befoer:',s0
-# s = copy.deepcopy(s0)
-# timeA = time.time()
-# s,coef = pyspline.compute_curve(s,X,knots,k,Nctl)
-# print 'Time is:',time.time()-timeA
-# # Check the rms
-# tot =0.0
-# for i in xrange(n):
-#     res = pyspline.eval_curve(s[i],knots,k,coef)
-#     tot = tot + (res[0] - X[i,0])**2 + (res[1] - X[i,1])**2
-# # end for
-# print 'rms:',sqrt(tot/n)
+Jac = pyspline.curve_jacobian_linear(Nctl,knots,k,s)
+global direct_time
+global trans_time 
+global direct_count
+global trans_count
+direct_time = 0.0
+trans_time = 0.0
+direct_count = 0
+trans_count = 0
+def aprod(mode,m,n,u):
+    '''Do the dot product'''
+    global direct_time
+    global trans_time 
+    global direct_count
+    global trans_count
 
-# sys.exit(0)
+    if mode == 1:
+        timeA = time.time()
+        val = dot(Jac,u)
+        direct_time += time.time()-timeA
+        direct_count += 1
+        return val
+
+#         coef[:,0] = u
+#         val = pyspline.eval_curve_v(s,knots,k,coef)
+#         return val[0]
+    else:
+        trans_count += 1
+        timeA = time.time()
+        val = dot(Jac.T,u)
+        trans_time += time.time()-timeA
+        return val
+    # end if
+
+nrow = n
+ncol = Nctl
+timeA = time.time()
+for i in xrange(100):
+    res = lsqr( nrow,ncol, aprod, X, 0, 1e-4,1e-4, 1e8, 100, 0)
+print 'lsqr time:',time.time()-timeA
+print 'direct_time:',direct_time
+print 'trans_time:',trans_time
+print 'counts:',direct_count,trans_count
+print 'x:',res[0]
+print 'res:',res[1:]
+
+timeB = time.time()
+for i in xrange(100):
+    res2 = lstsq(Jac,X[:,0])
+print 'lstsq time:',time.time()-timeB
+print 'res2:',res2[0]
+
+diff = res[0]-res2[0]
+print 'diff:',sqrt(dot(diff,diff))
+sys.exit(0)
+# Check the rms
+tot =0.0
+for i in xrange(n):
+    res = pyspline.eval_curve(s[i],knots,k,coef)
+    tot = tot + (res[0] - X[i,0])**2 + (res[1] - X[i,1])**2
+# end for
+print 'rms:',sqrt(tot/n)
+
+
+
+
+sys.exit(0)
 # coef = zeros((3,3))
 # coef[0,:] = [1,0,1]
 # coef[1,:] = [1,1,sqrt(2)/2]
