3b827d0186e65f8e48c5687b94514ef419db0c13
Gaetan Kenway <gaetank@gmail.com>
2010-03-16 20:16:58 +0000

    small modification

--- src/compute_curve.f90
+++ src/compute_curve.f90
@@ -1,111 +1,111 @@
-subroutine compute_curve(s,X,t,k,n,nctl,ndim,coef,niter,tol,rms)
+! subroutine compute_curve(s,X,t,k,n,nctl,ndim,coef,niter,tol,rms)
 
-  !***DESCRIPTION
-  !
-  !     Written by Gaetan Kenway
-  !
-  !     Abstract: compute_curve is the main function for the
-  !               generation fo B-spline curves It does both
-  !               interpolating and LMS fits, as well as Hoschek's 
-  !               Parameter Correction.
-  !
-  !     Description of Arguments
-  !     Input
-  !     s       - Real, Vector of s coordinates, length n
-  !     X       - Real, Array of X values to fit, Size (n,ndim)
-  !     t       - Real,Knot vector. Length nctl+k
-  !     k       - Integer,order of spline
-  !     nctl    - Integer,Number of control points
-  !     ndim    - Integer, spatial dimension of curve
-  !     niter   - Integer, number of hoscek para-correction iterations
-  !     tol     - Integer, relative tolerance for convergence
-  !
-  !     Ouput coef - Real,Array of B-spline coefficients. Size (nctl,ndim)
+!   !***DESCRIPTION
+!   !
+!   !     Written by Gaetan Kenway
+!   !
+!   !     Abstract: compute_curve is the main function for the
+!   !               generation fo B-spline curves It does both
+!   !               interpolating and LMS fits, as well as Hoschek's 
+!   !               Parameter Correction.
+!   !
+!   !     Description of Arguments
+!   !     Input
+!   !     s       - Real, Vector of s coordinates, length n
+!   !     X       - Real, Array of X values to fit, Size (n,ndim)
+!   !     t       - Real,Knot vector. Length nctl+k
+!   !     k       - Integer,order of spline
+!   !     nctl    - Integer,Number of control points
+!   !     ndim    - Integer, spatial dimension of curve
+!   !     niter   - Integer, number of hoscek para-correction iterations
+!   !     tol     - Integer, relative tolerance for convergence
+!   !
+!   !     Ouput coef - Real,Array of B-spline coefficients. Size (nctl,ndim)
 
-  use lms_jacobian
-  use  lsqrModule,        only : LSQR
-  use  lsqrCheckModule,   only : Acheck, xcheck
+!   use lms_jacobian
+!   use  lsqrModule,        only : LSQR
+!   use  lsqrCheckModule,   only : Acheck, xcheck
 
-  implicit none
+!   implicit none
   
-  ! Input
-  integer         , intent(in)          :: k,nctl,ndim,n
-  double precision, intent(in)          :: X(n,ndim)
-  double precision, intent(inout)       :: s(n)
-  double precision, intent(in)          :: t(nctl+k)
-  integer         , intent(in)          :: niter
-  double precision, intent(in)          :: tol
+!   ! Input
+!   integer         , intent(in)          :: k,nctl,ndim,n
+!   double precision, intent(in)          :: X(n,ndim)
+!   double precision, intent(inout)       :: s(n)
+!   double precision, intent(in)          :: t(nctl+k)
+!   integer         , intent(in)          :: niter
+!   double precision, intent(in)          :: tol
 
-  ! Output
-  double precision, intent(inout)       :: coef(nctl,ndim)
-  double precision, intent(out)         :: rms
-  ! Working
-  integer                               :: i,idim,iter
-  double precision                      :: weight
-  double precision                      :: length,res,norm,tot,val(ndim)
-  integer                               :: istop,itn
-  double precision                      :: Anorm,Acond,rnorm, Arnorm,xnorm
-  ! Functions called
-  double precision                      :: poly_length,floor,compute_rms_curve
+!   ! Output
+!   double precision, intent(inout)       :: coef(nctl,ndim)
+!   double precision, intent(out)         :: rms
+!   ! Working
+!   integer                               :: i,idim,iter
+!   double precision                      :: weight
+!   double precision                      :: length,res,norm,tot,val(ndim)
+!   integer                               :: istop,itn
+!   double precision                      :: Anorm,Acond,rnorm, Arnorm,xnorm
+!   ! Functions called
+!   double precision                      :: poly_length,floor,compute_rms_curve
 
-  !Initialization
-  length = poly_length(X,n,ndim)  
-  call setup_jacobian(n,nctl,k)
-  call curve_jacobian_linear(t,k,s,n,nctl)
+!   !Initialization
+!   length = poly_length(X,n,ndim)  
+!   call setup_jacobian(n,nctl,k)
+!   call curve_jacobian_linear(t,k,s,n,nctl)
 
-  do iter=1,niter
-     ! Solve
-     idim = 1
-     do idim=1,ndim
-        call LSQR( n, Nctl, Aprod1, Aprod2,X(:,idim),0.0, .False., &
-             coef(:,idim), vals, 1e-12, 1e-12, 1e8, Nctl*4,0,&
-             istop, itn, Anorm, Acond, rnorm, Arnorm, xnorm )
-     end do
-     call curve_para_corr(t,k,s,coef,nctl,ndim,length,n,X)
-     rms = compute_rms_curve(t,k,s,coef,nctl,ndim,length,n,X)
-     call curve_jacobian_linear(t,k,s,n,nctl)
-     ! Do convergence Check to break early
-  end do
+!   do iter=1,niter
+!      ! Solve
+!      idim = 1
+!      do idim=1,ndim
+!         call LSQR( n, Nctl, Aprod1, Aprod2,X(:,idim),0.0, .False., &
+!              coef(:,idim), vals, 1e-12, 1e-12, 1e8, Nctl*4,0,&
+!              istop, itn, Anorm, Acond, rnorm, Arnorm, xnorm )
+!      end do
+!      call curve_para_corr(t,k,s,coef,nctl,ndim,length,n,X)
+!      rms = compute_rms_curve(t,k,s,coef,nctl,ndim,length,n,X)
+!      call curve_jacobian_linear(t,k,s,n,nctl)
+!      ! Do convergence Check to break early
+!   end do
  
-  call kill_jacobian()
+!   call kill_jacobian()
 
-end subroutine compute_curve
+! end subroutine compute_curve
 
-subroutine curve_jacobian_linear(t,k,s,n,nctl)
-  use lms_jacobian
+! subroutine curve_jacobian_linear(t,k,s,n,nctl)
+!   use lms_jacobian
 
-  implicit none
-  ! Input
-  double precision     ,intent(in)      :: t(nctl+k)
-  double precision     ,intent(in)      :: s(n)
-  integer              ,intent(in)      :: k,nctl,n
-  ! Output -> in lms_jacobian module
+!   implicit none
+!   ! Input
+!   double precision     ,intent(in)      :: t(nctl+k)
+!   double precision     ,intent(in)      :: s(n)
+!   integer              ,intent(in)      :: k,nctl,n
+!   ! Output -> in lms_jacobian module
 
-  double precision                      :: vnikx(k),work(2*k)
-  integer                               :: i,j,counter
-  integer                               :: ilo,ileft,mflag,iwork
+!   double precision                      :: vnikx(k),work(2*k)
+!   integer                               :: i,j,counter
+!   integer                               :: ilo,ileft,mflag,iwork
   
-  ilo = 1
-  counter = 1
-  do i=1,n
-     call intrv(t,nctl+k,s(i),ilo,ileft,mflag)
-     if (mflag == 0) then
-        call bspvn(t,k,k,1,s(i),ileft,vnikx,work,iwork)
-     else if (mflag == 1) then
-        ileft = nctl
-        vnikx(:) = 0.0
-        vnikx(k) = 1.0
-     end if
+!   ilo = 1
+!   counter = 1
+!   do i=1,n
+!      call intrv(t,nctl+k,s(i),ilo,ileft,mflag)
+!      if (mflag == 0) then
+!         call bspvn(t,k,k,1,s(i),ileft,vnikx,work,iwork)
+!      else if (mflag == 1) then
+!         ileft = nctl
+!         vnikx(:) = 0.0
+!         vnikx(k) = 1.0
+!      end if
 
-     row_ptr(i) = counter
-     do j=1,k
-        col_ind(counter) = ileft-k+j
-        vals(counter) =vnikx(j)
-        counter = counter + 1
-     end do
-  end do
-  row_ptr(n+1) = counter
-end subroutine curve_jacobian_linear
+!      row_ptr(i) = counter
+!      do j=1,k
+!         col_ind(counter) = ileft-k+j
+!         vals(counter) =vnikx(j)
+!         counter = counter + 1
+!      end do
+!   end do
+!   row_ptr(n+1) = counter
+! end subroutine curve_jacobian_linear
 
 subroutine curve_jacobian_wrap(s,sd,t,k,nctl,n,nd,vals,row_ptr,col_ind)
   implicit none
