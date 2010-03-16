c9927e634b66c60eab0eac0467160f381216e118
kenway <kenway@localhost>
2010-03-16 20:16:58 +0000

    small modification

--- src/compute_surface.f90
+++ src/compute_surface.f90
@@ -1,163 +1,163 @@
-subroutine compute_surface(X,u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv,ndim,coef,niter,tol,rms)
-  !***DESCRIPTION
-  !
-  !     Written by Gaetan Kenway
-  !
-  !     Abstract: compute_curve is the main function for the
-  !               generation fo B-spline curves It does both
-  !               interpolating and LMS fits, as well as Hoschek's 
-  !               Parameter Correction.
-  !
-  !     Description of Arguments
-  !     Input
-  !     X       - Real, Array of X values to fit, Size (nu,nv,ndim)  
-  !     u       - Real, Array of u coordinates, Size nu x nv
-  !     v       - Real, Array of v coordinates, Size nu x nv
-  !     tu      - Real,Knot vector in u. Length nctlu+ku
-  !     tv      - Real,Knot vector in v. Length nctlv+kv
-  !     ku      - Integer, order of B-spline in u
-  !     kv      - Integer, order of B-spline in v
-  !     nctlu   - Integer,Number of control points in u
-  !     nctlv   - Integer,Number of control points in v
-  !     nu      - Integer, Number of data points in u
-  !     nv      - Integer, Number of data points in v
-  !     ndim    - Integer, spatial dimensions
-  !     coef    - Array, B-spline coefficients. Size nctlu x nctlv x ndim
-  !     niter   - Integer, number of hoscek para-correction iterations
-  !     tol     - Integer, relative tolerance for convergence
-  !
-  !     Ouput 
-  !     rms     - Real, final RMS value
+! subroutine compute_surface(X,u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv,ndim,coef,niter,tol,rms)
+!   !***DESCRIPTION
+!   !
+!   !     Written by Gaetan Kenway
+!   !
+!   !     Abstract: compute_curve is the main function for the
+!   !               generation fo B-spline curves It does both
+!   !               interpolating and LMS fits, as well as Hoschek's 
+!   !               Parameter Correction.
+!   !
+!   !     Description of Arguments
+!   !     Input
+!   !     X       - Real, Array of X values to fit, Size (nu,nv,ndim)  
+!   !     u       - Real, Array of u coordinates, Size nu x nv
+!   !     v       - Real, Array of v coordinates, Size nu x nv
+!   !     tu      - Real,Knot vector in u. Length nctlu+ku
+!   !     tv      - Real,Knot vector in v. Length nctlv+kv
+!   !     ku      - Integer, order of B-spline in u
+!   !     kv      - Integer, order of B-spline in v
+!   !     nctlu   - Integer,Number of control points in u
+!   !     nctlv   - Integer,Number of control points in v
+!   !     nu      - Integer, Number of data points in u
+!   !     nv      - Integer, Number of data points in v
+!   !     ndim    - Integer, spatial dimensions
+!   !     coef    - Array, B-spline coefficients. Size nctlu x nctlv x ndim
+!   !     niter   - Integer, number of hoscek para-correction iterations
+!   !     tol     - Integer, relative tolerance for convergence
+!   !
+!   !     Ouput 
+!   !     rms     - Real, final RMS value
 
-  use lms_jacobian
-  use lsqrModule,        only : LSQR
-  use lsqrCheckModule,   only : Acheck, xcheck
+!   use lms_jacobian
+!   use lsqrModule,        only : LSQR
+!   use lsqrCheckModule,   only : Acheck, xcheck
 
-  implicit none
+!   implicit none
   
-  ! Input/Output
-  double precision, intent(in)          :: X(nu,nv,ndim)
-  integer         , intent(in)          :: ku,kv,nctlu,nctlv,nu,nv,ndim
-  double precision, intent(inout)       :: u(nu,nv),v(nu,nv)
-  double precision, intent(in)          :: tu(nctlu+ku),tv(nctlv+kv)
-  double precision, intent(inout)       :: coef(nctlu,nctlv,ndim)
-  integer         , intent(in)          :: niter
-  double precision, intent(in)          :: tol
-  double precision, intent(out)         :: rms
+!   ! Input/Output
+!   double precision, intent(in)          :: X(nu,nv,ndim)
+!   integer         , intent(in)          :: ku,kv,nctlu,nctlv,nu,nv,ndim
+!   double precision, intent(inout)       :: u(nu,nv),v(nu,nv)
+!   double precision, intent(in)          :: tu(nctlu+ku),tv(nctlv+kv)
+!   double precision, intent(inout)       :: coef(nctlu,nctlv,ndim)
+!   integer         , intent(in)          :: niter
+!   double precision, intent(in)          :: tol
+!   double precision, intent(out)         :: rms
 
-  ! Working
-  integer                               :: i,idim,iter
-  integer                               :: istop,itn
-  double precision                      :: Anorm,Acond,rnorm, Arnorm,xnorm
-  ! Functions called
-  double precision                      :: poly_length,floor,compute_rms_surface
+!   ! Working
+!   integer                               :: i,idim,iter
+!   integer                               :: istop,itn
+!   double precision                      :: Anorm,Acond,rnorm, Arnorm,xnorm
+!   ! Functions called
+!   double precision                      :: poly_length,floor,compute_rms_surface
 
-  !Initialization
-  call setup_jacobian(nu*nv,nctlu*nctlv,ku*kv)
-  call surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
-  rms = 0.0
+!   !Initialization
+!   call setup_jacobian(nu*nv,nctlu*nctlv,ku*kv)
+!   call surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
+!   rms = 0.0
 
 
-  do iter=1,niter
-!      ! Solve
-      idim = 1
-      do idim=1,ndim
-         call LSQR( nu*nv, Nctlu*Nctlv, Aprod1, Aprod2,X(:,:,idim),.0, .False., &
-              coef(:,:,idim), vals, 1e-12, 1e-12, 1e8, Nctlu*Nctlv*10,0,&
-              istop, itn, Anorm, Acond, rnorm, Arnorm, xnorm )
-         print *,'istop,itn:',istop,itn
-         print *,'Anorm,Acond:',Anorm,Acond,Arnorm
+!   do iter=1,niter
+! !      ! Solve
+!       idim = 1
+!       do idim=1,ndim
+!          call LSQR( nu*nv, Nctlu*Nctlv, Aprod1, Aprod2,X(:,:,idim),.0, .False., &
+!               coef(:,:,idim), vals, 1e-12, 1e-12, 1e8, Nctlu*Nctlv*10,0,&
+!               istop, itn, Anorm, Acond, rnorm, Arnorm, xnorm )
+!          print *,'istop,itn:',istop,itn
+!          print *,'Anorm,Acond:',Anorm,Acond,Arnorm
 
-      end do
-      !call surface_para_corr(t,k,s,coef,nctl,ndim,length,n,X)
-      rms = compute_rms_surface(tu,tv,ku,kv,u,v,coef,nctlu,nctlv,ndim,nu,nv,X)
-      call surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
-     ! Do convergence Check to break early
+!       end do
+!       !call surface_para_corr(t,k,s,coef,nctl,ndim,length,n,X)
+!       rms = compute_rms_surface(tu,tv,ku,kv,u,v,coef,nctlu,nctlv,ndim,nu,nv,X)
+!       call surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
+!      ! Do convergence Check to break early
 
-  end do
-  call kill_jacobian()
+!   end do
+!   call kill_jacobian()
 
-end subroutine compute_surface
+! end subroutine compute_surface
 
-subroutine surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
+! subroutine surface_jacobian_linear(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv)
 
-  !***DESCRIPTION
-  !
-  !     Written by Gaetan Kenway
-  !
-  !     Abstract surface_jacobian_linear computes jacobian in rcs storage
-  !
-  !     Description of Arguments
-  !     Input
-  !     u       - Real, u coordinates, size: nu x nv
-  !     v       - Real, v coordinates, size: nu x nv
-  !     tu      - Real,Knot vector in u. Length nctlu+ku
-  !     tv      - Real,Knot vector in v. Length nctlv+kv
-  !     ku      - Integer, order of B-spline in u
-  !     kv      - Integer, order of B-spline in v
-  !     nctlu   - Integer,Number of control points in u
-  !     nctlv   - Integer,Number of control points in v
-  !     nu      - Integer, Number of data points in u
-  !     nv      - Integer, Number of data points in v
-  !
-  !     Ouput -> lms_jacobian module
+!   !***DESCRIPTION
+!   !
+!   !     Written by Gaetan Kenway
+!   !
+!   !     Abstract surface_jacobian_linear computes jacobian in rcs storage
+!   !
+!   !     Description of Arguments
+!   !     Input
+!   !     u       - Real, u coordinates, size: nu x nv
+!   !     v       - Real, v coordinates, size: nu x nv
+!   !     tu      - Real,Knot vector in u. Length nctlu+ku
+!   !     tv      - Real,Knot vector in v. Length nctlv+kv
+!   !     ku      - Integer, order of B-spline in u
+!   !     kv      - Integer, order of B-spline in v
+!   !     nctlu   - Integer,Number of control points in u
+!   !     nctlv   - Integer,Number of control points in v
+!   !     nu      - Integer, Number of data points in u
+!   !     nv      - Integer, Number of data points in v
+!   !
+!   !     Ouput -> lms_jacobian module
 
-  use lms_jacobian
+!   use lms_jacobian
 
-  implicit none
-  ! Input
-  integer         , intent(in)          :: ku,kv,nctlu,nctlv,nu,nv
-  double precision, intent(in)          :: u(nu,nv),v(nu,nv)
-  double precision, intent(in)          :: tu(nctlu+ku),tv(nctlv+kv)
+!   implicit none
+!   ! Input
+!   integer         , intent(in)          :: ku,kv,nctlu,nctlv,nu,nv
+!   double precision, intent(in)          :: u(nu,nv),v(nu,nv)
+!   double precision, intent(in)          :: tu(nctlu+ku),tv(nctlv+kv)
 
-  ! Working
-  double precision                      :: vniku(ku),worku(2*ku)
-  integer                               :: ilou,ileftu,mflagu
+!   ! Working
+!   double precision                      :: vniku(ku),worku(2*ku)
+!   integer                               :: ilou,ileftu,mflagu
 
-  double precision                      :: vnikv(kv),workv(2*kv)
-  integer                               :: ilov,ileftv,mflagv
+!   double precision                      :: vnikv(kv),workv(2*kv)
+!   integer                               :: ilov,ileftv,mflagv
 
-  integer                               :: i,j,ii,jj,counter,iwork
+!   integer                               :: i,j,ii,jj,counter,iwork
 
-  ilou = 1
-  ilov = 1
-  counter = 1
-  do i=1,nu
-     do j = 1,nv
-        ! Get u interval
-        call intrv(tu,nctlu+ku,u(i,j),ilou,ileftu,mflagu)
-        if (mflagu == 0) then
-           call bspvn(tu,ku,ku,1,u(i,j),ileftu,vniku,worku,iwork)
-        else if (mflagu == 1) then
-           ileftu = nctlu
-           vniku(:) = 0.0
-           vniku(ku) = 1.0
-        end if
+!   ilou = 1
+!   ilov = 1
+!   counter = 1
+!   do i=1,nu
+!      do j = 1,nv
+!         ! Get u interval
+!         call intrv(tu,nctlu+ku,u(i,j),ilou,ileftu,mflagu)
+!         if (mflagu == 0) then
+!            call bspvn(tu,ku,ku,1,u(i,j),ileftu,vniku,worku,iwork)
+!         else if (mflagu == 1) then
+!            ileftu = nctlu
+!            vniku(:) = 0.0
+!            vniku(ku) = 1.0
+!         end if
 
-        ! Get v interval
-        call intrv(tv,nctlv+kv,v(i,j),ilov,ileftv,mflagv)
-        if (mflagv == 0) then
-           call bspvn(tv,kv,kv,1,v(i,j),ileftv,vnikv,workv,iwork)
-        else if (mflagv == 1) then
-           ileftv = nctlv
-           vnikv(:) = 0.0
-           vnikv(kv) = 1.0
-        end if
+!         ! Get v interval
+!         call intrv(tv,nctlv+kv,v(i,j),ilov,ileftv,mflagv)
+!         if (mflagv == 0) then
+!            call bspvn(tv,kv,kv,1,v(i,j),ileftv,vnikv,workv,iwork)
+!         else if (mflagv == 1) then
+!            ileftv = nctlv
+!            vnikv(:) = 0.0
+!            vnikv(kv) = 1.0
+!         end if
         
-        row_ptr( (i-1)*nv + j ) = counter
-        do ii=1,ku
-           do jj = 1,kv
-              !rows(counter) = (i-1)*nv + j -1
-              ! cols(counter) = (ileftu-ku+ii-1)*Nctlv + (ileftv-kv+jj-1)
-              col_ind(counter) = (ileftu-ku+ii-1)*Nctlv + (ileftv-kv+jj)
-              vals(counter) = vniku(ii)*vnikv(jj)
-              counter = counter + 1
-           end do
-        end do
-     end do
-  end do
-  row_ptr(nu*nv+1) = counter
-end subroutine surface_jacobian_linear
+!         row_ptr( (i-1)*nv + j ) = counter
+!         do ii=1,ku
+!            do jj = 1,kv
+!               !rows(counter) = (i-1)*nv + j -1
+!               ! cols(counter) = (ileftu-ku+ii-1)*Nctlv + (ileftv-kv+jj-1)
+!               col_ind(counter) = (ileftu-ku+ii-1)*Nctlv + (ileftv-kv+jj)
+!               vals(counter) = vniku(ii)*vnikv(jj)
+!               counter = counter + 1
+!            end do
+!         end do
+!      end do
+!   end do
+!   row_ptr(nu*nv+1) = counter
+! end subroutine surface_jacobian_linear
 
 
 subroutine surface_jacobian_wrap(u,v,tu,tv,ku,kv,nctlu,nctlv,nu,nv,vals,row_ptr,col_ind)
