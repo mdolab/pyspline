87b09d1fda544c7be01760c2374ba579644a1398
Gaetan Kenway <gaetank@gmail.com>
2011-05-28 11:12:19 -0400

    update to build system

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -27,6 +27,6 @@ EXTRA_LIBS = -limf -lifcore
 # Combine Flags
 # ------------------------------------
 
-FF90_FLAGS = $(FFLAGS) $(EXTRA_LIBS)
+FF90_FLAGS = $(FFLAGS) $(USE_CGNS) $(USE_TECIO) $(CGNS_INCLUDE) 
 CC_FLAGS   = $(CFLAGS)
 USER_LIBS = $(CGNS_LIB) $(TEC_LIB) $(LIBSTDCpp) $(EXTRA_LIBS)
