bfea511c7e6de6c9e8e72a9d8a9185d36048dd0d
kenway <kenway@localhost>
2011-05-28 10:56:29 -0400

    Redid the makefile system to match that of SUmb,pyWarp,pyFriction etc

---
+++ config/defaults/config.LINUX_INTEL.mk
@@ -0,0 +1,32 @@
+# Config File for LINUX and INTEL Compiler
+AR       = ar
+AR_FLAGS = -rvs
+RM       = /bin/rm -rf
+
+FF90     = ifort
+FFLAGS    =  -r8 -O2 -fPIC
+
+CC       = gcc
+CFLAGS   = -O2 -fPIC
+
+COMPILER_NAME = intel
+EXTRA_LIBS = -limf -lifcore
+
+# Optional Things
+
+# CGNS Functionality
+#USE_CGNS = -DUSE_CGNS
+#CGNS_LIB = -lcgns
+#CGNS_INCLUDE = -I/usr/local/include/
+
+# Tecplot Binary IO
+# USE_TECIO = -DUSE_TECIO 
+# TEC_LIB   = ../tecio/tecio.a
+# LIBSTDCpp = -lstdc++
+
+# Combine Flags
+# ------------------------------------
+
+FF90_FLAGS = $(FFLAGS) $(EXTRA_LIBS)
+CC_FLAGS   = $(CFLAGS)
+USER_LIBS = $(CGNS_LIB) $(TEC_LIB) $(LIBSTDCpp) $(EXTRA_LIBS)
