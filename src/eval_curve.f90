2b7dd3a969d0a1ba22c34367e6dad43b7a234b53
kenway <kenway@localhost>
2012-02-13 14:15:47 -0500

    update to examples and added complex evaluations for the first and second curve derivatives

--- src/eval_curve.f90
+++ src/eval_curve.f90
@@ -205,3 +205,97 @@ subroutine eval_curve_c(s,t,k,coef,nctl,ndim,n,val)
      end do
   end do
 end subroutine eval_curve_c
+
+subroutine eval_curve_deriv_c(s,t,k,coef,nctl,ndim,val)
+
+  !***DESCRIPTION
+  !
+  !     Written by Gaetan Kenway
+  !
+  !     Abstract eval_surf_deriv is a scalar version of the 
+  !              B-spline derivative evaluation function
+  !
+  !     Description of Arguments
+  !     Input
+  !     s       - Real, s coordinate
+  !     t       - Real,Knot vector. Length nctl+k
+  !     k       - Integer,order of B-spline
+  !     coef    - Real,Array of B-spline coefficients. Size (ndim,nctl)
+  !     nctl   - Integer,Number of control points
+  !
+  !     Ouput 
+  !     val     - Real, Evaluated point, size ndim
+  
+  implicit none
+  ! Input
+  integer   , intent(in)          :: k,nctl,ndim
+  complex*16, intent(in)          :: s
+  complex*16, intent(in)          :: t(nctl+k)
+  complex*16, intent(in)          :: coef(ndim,nctl)
+
+  ! Output
+  complex*16, intent(out)         :: val(ndim)
+
+  ! Working
+  integer                         :: idim,inbv
+  complex*16                      :: work(3*k)
+
+  ! Functions
+  complex*16                      :: bvalu
+
+  inbv = 1
+  
+  do idim=1,ndim
+     val(idim) = bvalu(t,coef(idim,:),nctl,k,1,s,inbv,work)
+  end do
+    
+end subroutine eval_curve_deriv_c
+
+subroutine eval_curve_deriv2_c(s,t,k,coef,nctl,ndim,val)
+
+  !***DESCRIPTION
+  !
+  !     Written by Gaetan Kenway
+  !
+  !     Abstract eval_curve_deriv2 evals the 2nd derivative
+  !              B-spline derivative evaluation function
+  !
+  !     Description of Arguments
+  !     Input
+  !     s       - Real, s coordinate
+  !     t       - Real,Knot vector. Length nctl+k
+  !     k       - Integer,order of B-spline
+  !     coef    - Real,Array of B-spline coefficients Size (ndim,nctl)
+  !     nctl    - Integer,Number of control points
+  !
+  !     Ouput 
+  !     val     - Real, Evaluated point, size ndim
+  
+  implicit none
+  ! Input
+  integer   , intent(in)          :: k,nctl,ndim
+  complex*16, intent(in)          :: s
+  complex*16, intent(in)          :: t(nctl+k)
+  complex*16, intent(in)          :: coef(ndim,nctl)
+
+  ! Output
+  complex*16, intent(out)         :: val(ndim)
+
+  ! Working
+  integer                         :: idim,inbv
+  complex*16                      :: work(3*k)
+
+  ! Functions
+  complex*16                      :: bvalu
+
+  inbv = 1
+  
+  if (k == 2) then
+     val(:) = 0.0
+  else
+     do idim=1,ndim
+        val(idim) = bvalu(t,coef(idim,:),nctl,k,2,s,inbv,work)
+     end do
+  end if
+
+end subroutine eval_curve_deriv2_c
