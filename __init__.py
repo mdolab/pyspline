617f5f39ec730419881f5330ff0efc8e0d1a3618
Gaetan Kenway <gaetank@gmail.com>
2013-09-24 15:00:00 -0400

    fixed bug casued by the .so and package name being the same

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import sys, os
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(cur_dir, 'python')
+sys.path.append(os.path.join(cur_dir, 'python'))
 import pySpline
 __all__ = ['pySpline']
