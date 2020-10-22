28d9a5e10a1e1517d5bce28ae8e66df877d4857c
Marco Mangano <mmangano@umich.edu>
2020-10-22 12:28:52 +0200

    switch to sphinx_mdolab_theme and added to requirements.txt

--- doc/conf.py
+++ doc/conf.py
@@ -17,39 +17,39 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'numpydoc']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "numpydoc"]
 
 # mock import for autodoc
-autodoc_mock_imports = ['numpy', 'scipy', 'pyspline.libspline']
+autodoc_mock_imports = ["numpy", "scipy", "pyspline.libspline"]
 
 numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'pySpline'
-copyright = '2020, MDO Lab'
+project = "pySpline"
+copyright = "2020, MDO Lab"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_mdolab_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
