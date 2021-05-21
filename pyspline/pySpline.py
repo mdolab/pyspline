4a5c2a6f6c63577aa78ce248e09d2b635e7801d5
Neil Wu <neilwu@umich.edu>
2021-05-21 13:14:31 -0400

    added noqa

--- pyspline/pySpline.py
+++ pyspline/pySpline.py
@@ -8,10 +8,10 @@ Contains classes for working with B-spline :class:`Curve`, :class:`Surface` and
 
 # Local modules
 from . import libspline  # noqa: F401
-from .pyCurve import Curve
-from .pySurface import Surface
-from .pyVolume import Volume
-from .utils import (
+from .pyCurve import Curve  # noqa: F401
+from .pySurface import Surface  # noqa: F401
+from .pyVolume import Volume  # noqa: F401
+from .utils import (  # noqa: F401
     bilinearSurface,
     checkInput,
     closeTecplot,
