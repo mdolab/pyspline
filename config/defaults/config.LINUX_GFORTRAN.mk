aaf5de716764d3dc7079c373ed0e4e1794c9be89
Neil Wu <neilwu@umich.edu>
2021-07-04 11:07:44 -0400

    added -std=f95 flag

--- config/defaults/config.LINUX_GFORTRAN.mk
+++ config/defaults/config.LINUX_GFORTRAN.mk
@@ -5,7 +5,7 @@ RM       = /bin/rm -rf
 
 # Fortran compiler and flags
 FF90        = gfortran
-FF90_FLAGS  = -fdefault-real-8 -O2 -fPIC
+FF90_FLAGS  = -fdefault-real-8 -O2 -fPIC -std=f95
 
 # C compiler and flags
 CC       = gcc
