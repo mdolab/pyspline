ab61d6d5f8f64976ae484175858b0d45bd3b206a
Alex Kleb <akleb@umich.edu>
2022-05-06 17:10:03 -0400

    use intel compilers on that image

--- .github/build_real.sh
+++ .github/build_real.sh
@@ -2,7 +2,13 @@
 set -e
 mkdir build/
 cd build/
-cmake ../
+if [[ $COMPILERS == "intel" ]]; then
+	cmake -DCMAKE_CXX_COMPILER=icpc \
+		  -DCMAKE_C_COMPILER=icc \
+		  -DCMAKE_Fortran_COMPILER=ifort ../
+else
+	cmake ../
+fi
 make check
 make install
 cd ../
