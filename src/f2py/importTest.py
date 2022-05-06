97657d292cc2d1b3b89cd3597db74df9d48006a7
Alex Kleb <akleb@umich.edu>
2022-05-06 17:10:18 -0400

    use importlib for importTest

--- src/f2py/importTest.py
+++ src/f2py/importTest.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python
 # Standard Python modules
-from ast import Import
+import importlib.util
+import os
 import sys
 
 print("Testing if module pyspline can be imported...")
 try:
-    # External modules
-    import libspline  # noqa: F401
+    # Checks for the libspline module in current working directory
+    cwd = os.getcwd()
+    spec = importlib.util.spec_from_file_location("libspline", os.path.join(cwd, "libspline.so"))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 except ImportError as e:
     print("Error importing libspline.so")
     print(e)
