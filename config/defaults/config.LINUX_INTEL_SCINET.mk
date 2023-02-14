a3b8e7a7834d00f64753828160627268608e56e7
Eirikur Jonsson <eirikurj@umich.edu>
2023-02-14 11:02:13 +0000

    change CC compiler to icc for Intel defaults

--- config/defaults/config.LINUX_INTEL_SCINET.mk
+++ config/defaults/config.LINUX_INTEL_SCINET.mk
@@ -8,7 +8,7 @@ FF90        = ifort
 FF90_FLAGS  = -r8 -O2 -fPIC
 
 # C compiler and flags
-CC       = gcc
+CC       = icc
 CC_FLAGS   = -O2 -fPIC
 
 # Define potentially different python, python-config and f2py executables:
@@ -17,5 +17,5 @@ PYTHON-CONFIG = python3-config # use python-config for python 2
 F2PY = f2py
 
 # Define additional flags for linking
-LINKER_FLAGS = 
+LINKER_FLAGS =
 SO_LINKER_FLAGS =-fPIC -shared
