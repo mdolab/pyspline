0b7d90589651999e536c259477bf4aac327a03ec
Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
2023-02-21 17:40:21 +0000

    Change CC compiler to icc for Intel config (#55)
    * change CC compiler to icc for Intel defaults
    
    * adding -std=c99 flag to config to support newer numpy/f2py
    
    ---------
    
    Co-authored-by: Anil Yildirim <42349285+anilyil@users.noreply.github.com>

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -8,8 +8,8 @@ FF90        = ifort
 FF90_FLAGS  = -r8 -O2 -fPIC -stand f08
 
 # C compiler and flags
-CC       = gcc
-CC_FLAGS   = -O2 -fPIC
+CC       = icc
+CC_FLAGS   = -O2 -fPIC -std=c99
 
 # Define potentially different python, python-config and f2py executables:
 PYTHON = python
@@ -17,5 +17,5 @@ PYTHON-CONFIG = python3-config # use python-config for python 2
 F2PY = f2py
 
 # Define additional flags for linking
-LINKER_FLAGS = 
+LINKER_FLAGS =
 SO_LINKER_FLAGS =-fPIC -shared
