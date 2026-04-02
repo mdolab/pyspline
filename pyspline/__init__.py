79283aed1970886869bf06df94c242091c7e7be0
mpdutta <mpdutta@umich.edu>
2026-04-02 11:57:51 -0400

    Updating dependencies and bumping version (#78)

--- pyspline/__init__.py
+++ pyspline/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 
 from .pyCurve import Curve
 from .pySurface import Surface
--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
     license="Apache License Version 2.0",
     packages=["pyspline"],
     package_data={"pyspline": ["*.so"]},
-    install_requires=["numpy>=1.21", "scipy>=1.7"],
+    python_requires=">=3.11",
+    install_requires=["numpy>=1.25", "scipy>=1.11"],
     extras_require={
         "docs": docs_require,
         "testing": ["parameterized", "testflo>=1.4.5", "mdolab-baseclasses>=1.6.1"],
